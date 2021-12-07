6d07eeb393ba69d0f59ec7c4e30cc112832b5b6a
Christelle <ck.degio@yahoo.fr>
2021-12-07 16:39:33 +0100

    amelioration login

--- routes/index.js
+++ routes/index.js
@@ -17,41 +17,65 @@ router.post("/sign-up", async function (req, res, next) {
   console.log("req.body.usernameFromFront", req.body.username);
   console.log("req.body.emailFromFront", req.body.email);
   console.log("req.body.passwordFromFront", req.body.password);
-  var newUser = new UserModel({
-    username: req.body.username,
+  var error = [];
+  var result = false;
+  var saveUser = null;
+  var token = null;
+
+  const data = await UserModel.findOne({
     email: req.body.email,
-    password: req.body.password,
-    token: "azert",
   });
 
-  var userSaved = await newUser.save();
+  if (data !== null) {
+    error.push("utilisateur déjà présent");
+  }
 
-  // var result = false;
-  // var user = null;
-  // var error = [];
-  // var token = null;
-  // if (req.body.emailFromFront === "" || req.body.passwordFromFront === "") {
-  //   error.push("champs vides");
-  // }
-  // if (error.length === 0) {
-  //   user = await UserModel.findOne({
-  //     email: req.body.emailFromFront,
-  //   });
-  //   if (user) {
-  //     if (bcrypt.compareSync(req.body.passwordFromFront, user.password)) {
-  //       result = true;
-  //       token = user.token;
-  //     } else {
-  //       result = false;
-  //       error.push("mot de passe incorrect");
-  //     }
-  //   } else {
-  //     error.push("email incorrect");
-  //   }
-  // }
-  // res.json({ result, user, error, token });
-  res.json({ userSaved });
+  if (
+    req.body.username === "" ||
+    req.body.email === "" ||
+    req.body.password === ""
+  ) {
+    error.push("champs vides");
+  }
+
+  if (error.length === 0) {
+    var hash = bcrypt.hashSync(req.body.password, 10);
+    var newUser = new UserModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+      token: uid2(32),
+    });
+    console.log("newUser:user+mail+password+token", newUser);
+
+    saveUser = await newUser.save();
+
+    if (saveUser) {
+      result = true;
+      token = saveUser.token;
+    }
+  }
+
+  res.json({ result, saveUser, error, token });
+  console.log("token", res.json.token);
 });
+
+// router.post("/sign-up", async function (req, res, next) {
+//   console.log("req.body", req.body);
+//   console.log("req.body.usernameFromFront", req.body.username);
+//   console.log("req.body.emailFromFront", req.body.email);
+//   console.log("req.body.passwordFromFront", req.body.password);
+//   var newUser = new UserModel({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password,
+//     token: "azert",
+//   });
+
+//   var userSaved = await newUser.save();
+
+//   res.json({ userSaved });
+// });
 
 /* POST SIGN IN*/
 router.post("/sign-in", async function (req, res, next) {
