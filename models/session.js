870d399723a66e4d664f6f5a743b73233eb33a29
Christelle <ck.degio@yahoo.fr>
2021-12-08 06:02:29 +0100

    modif session model

--- models/session.js
+++ models/session.js
@@ -1,14 +1,14 @@
 var mongoose = require("mongoose");
 
 var sessionSchema = mongoose.Schema({
-  date: Date
+  date: Date,
   location: {
-      long: Number,
-      lat: Number
-  }
+    long: Number,
+    lat: Number,
+  },
 });
 
-var SessionModel = mongoose.model("session",sessionSchema);
+var SessionModel = mongoose.model("session", sessionSchema);
 console.log("Sessionmodel from models", SessionModel);
 
 module.exports = SessionModel;
