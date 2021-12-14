c45ee92981d73db8ddff105b4f5b304655e28040
Christelle <ck.degio@yahoo.fr>
2021-12-14 09:02:23 +0100

    regul

--- routes/index.js
+++ routes/index.js
@@ -234,16 +234,3 @@ router.get("/searchScreen", async function (req, res, next) {
 });
 
 module.exports = router;
-
-/* GET journal page historique */
-router.get("/journal", async function (req, res, next) {
-  console.log("GET Journal userSession", req.query);
-  var user = await UserModel.findOne({ token: req.query.token });
-  console.log("ID recupéré par Token", user);
-  var userHistorique = await SessionModel.find({
-    $or: [{ creatorId: user._id }, { buddyId: user._id }],
-  });
-  console.log("userHistorique", userHistorique);
-
-  res.json({ userHistorique });
-});
